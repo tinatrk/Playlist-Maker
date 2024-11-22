78d3c7d14fe00e0603b9d8d1384801c1b6ca717f
Tatyana Soloveva android_plus 24 <tatjana.solov2014@yandex.ru>
2024-11-22 18:03:47 +0300

    Домашняя работа 3 (добавлены переходы между экранами)

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,15 +1,50 @@
 package com.example.playlistmaker
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.widget.SwitchCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        enableEdgeToEdge()
         setContentView(R.layout.activity_settings)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.settings_screen)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val btnBack = findViewById<ImageView>(R.id.img_view_back_settings_screen)
+        btnBack.setOnClickListener{
+            val intent = Intent(this@SettingsActivity, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        val themeSwitch = findViewById<SwitchCompat>(R.id.switch_mode)
+        val sharedPreferences = getSharedPreferences("save",MODE_PRIVATE)
+        themeSwitch.setChecked(sharedPreferences.getBoolean("value",true))
+
+        themeSwitch.setOnClickListener {
+            if (themeSwitch.isChecked) {
+                val editor = getSharedPreferences("save", MODE_PRIVATE).edit()
+                editor.putBoolean("value", true)
+                editor.apply()
+                themeSwitch.setChecked(true)
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            } else {
+                val editor = getSharedPreferences("save", MODE_PRIVATE).edit()
+                editor.putBoolean("value", false)
+                editor.apply()
+                themeSwitch.setChecked(false)
+                AppCompatDelegate.setDefaultNightMode (AppCompatDelegate.MODE_NIGHT_NO)
+            }
+        }
     }
 }
