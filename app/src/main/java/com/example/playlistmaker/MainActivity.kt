380434d8c320443001372c8075e342fc7985ab60
Tatyana Soloveva android_plus 24 <tatjana.solov2014@yandex.ru>
2024-11-22 18:03:47 +0300

    Домашняя работа 3 (добавлены переходы между экранами)

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,13 +1,9 @@
 package com.example.playlistmaker
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
-import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,27 +11,21 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val btnSearch = findViewById<Button>(R.id.btn_search)
-        val btnClickListener: View.OnClickListener = object: View.OnClickListener{
-            override fun onClick(v: View?) {
-                Toast.makeText(this@MainActivity, "Вы нажали на кнопку " +
-                        "\"${resources.getString(R.string.search_button_text)}\"!",
-                    Toast.LENGTH_SHORT).show()
-            }
+        btnSearch.setOnClickListener{
+            val intent = Intent(this@MainActivity, SearchActivity::class.java)
+            startActivity(intent)
         }
-        btnSearch.setOnClickListener(btnClickListener)
 
         val btnLibrary = findViewById<Button>(R.id.btn_library)
         btnLibrary.setOnClickListener{
-            Toast.makeText(this@MainActivity, "Вы нажали на кнопку " +
-                    "\"${resources.getString(R.string.library_button_text)}\"",
-                Toast.LENGTH_SHORT).show()
+            val intent = Intent(this@MainActivity, LibraryActivity::class.java)
+            startActivity(intent)
         }
 
         val btnSettings = findViewById<Button>(R.id.btn_settings)
         btnSettings.setOnClickListener{
-            Toast.makeText(this@MainActivity, "Вы нажали на кнопку " +
-                    "\"${resources.getString(R.string.settings_button_text)}\"",
-                Toast.LENGTH_SHORT).show()
+            val intent = Intent(this@MainActivity, SettingsActivity::class.java)
+            startActivity(intent)
         }
     }
 }
