ba18622e1ba9fdb0d23798a102c7225ccab25515
Tatyana Soloveva <102318806+tinatrk@users.noreply.github.com>
2024-12-10 13:20:26 +0300

    Домашняя работа спринт 9 (#4)
    Домашняя работа (1, 2, 3, 4) спринт 9

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -4,11 +4,18 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main_screen)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
 
         val btnSearch = findViewById<Button>(R.id.btn_search)
         btnSearch.setOnClickListener{
--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,14 +1,25 @@
 package com.example.playlistmaker
 
-import android.content.Intent
+import android.content.Context
 import android.os.Bundle
+import android.text.Editable
 import android.widget.ImageView
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.isVisible
 
 class SearchActivity : AppCompatActivity() {
+
+    private var searchLineText = STRING_DEF_VALUE
+    private var searchLineHasFocus: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -19,9 +30,72 @@ class SearchActivity : AppCompatActivity() {
             insets
         }
 
-        val btnBack = findViewById<ImageView>(R.id.img_view_back_search_screen)
-        btnBack.setOnClickListener{
+        val toolbar = findViewById<Toolbar>(R.id.toolbar_search_screen)
+        toolbar.setNavigationOnClickListener{
             finish()
         }
+
+        val searchLine = findViewById<EditText>(R.id.search_line)
+        searchLine.setOnFocusChangeListener { v, hasFocus -> searchLineHasFocus = hasFocus}
+
+        val clearButton = findViewById<ImageView>(R.id.clearIcon_search_line)
+
+        clearButton.setOnClickListener {
+            searchLine.setText(STRING_DEF_VALUE)
+            val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+            inputMethodManager?.hideSoftInputFromWindow(searchLine.windowToken, 0)
+            searchLine.clearFocus()
+        }
+
+        val textWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // empty
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                clearButton.isVisible = !s.isNullOrEmpty()
+                searchLineText = s.toString()
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                // empty
+            }
+        }
+        searchLine.addTextChangedListener(textWatcher)
+
     }
+
+    private fun clearButtonVisibility(s: CharSequence?): Int {
+        return if (s.isNullOrEmpty()) {
+            View.GONE
+        } else {
+            View.VISIBLE
+        }
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        searchLineText = savedInstanceState.getString(SEARCH_LINE_TEXT, STRING_DEF_VALUE)
+        val searchLine = findViewById<EditText>(R.id.search_line)
+        searchLine.setText(searchLineText)
+        searchLineHasFocus = savedInstanceState.getBoolean(SEARCH_LINE_HAS_FOCUS, false)
+        if (searchLineHasFocus){
+            searchLine.setSelection(searchLine.length())
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+            imm?.showSoftInput(searchLine, InputMethodManager.SHOW_IMPLICIT)
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(SEARCH_LINE_TEXT, searchLineText)
+        outState.putBoolean(SEARCH_LINE_HAS_FOCUS, searchLineHasFocus)
+    }
+
+    private companion object{
+        const val SEARCH_LINE_TEXT = "SEARCH_LINE_TEXT"
+        const val STRING_DEF_VALUE = ""
+        const val SEARCH_LINE_HAS_FOCUS = "SEARCH_LINE_HAS_FOCUS"
+    }
+
 }
